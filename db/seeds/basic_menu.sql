b6eec789f0d3ba7ba744e3794344e299056d534f
James Silberbauer <jamessil@telkomsa.net>
2017-10-17 08:06:34 +0200

    Add developer menu items to seeds.

--- db/seeds/basic_menu.sql
+++ db/seeds/basic_menu.sql
@@ -20,3 +20,26 @@ VALUES ((SELECT id FROM programs WHERE program_name = 'reports' AND functional_a
 
 INSERT INTO program_functions (program_id, program_function_name, url, program_function_sequence)
 VALUES ((SELECT id FROM programs WHERE program_name = 'reports' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'dataminer')), 'admin', '/dataminer/admin/', 2);
+
+
+INSERT INTO functional_areas (functional_area_name)
+VALUES ('Development');
+
+INSERT INTO programs (program_name, program_sequence, functional_area_id)
+VALUES ('Generators', 1, (SELECT id FROM functional_areas WHERE functional_area_name = 'Development'));
+
+INSERT INTO program_functions (program_id, program_function_name, url, program_function_sequence)
+VALUES ((SELECT id FROM programs WHERE program_name = 'Generators' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Development')), 'New Scaffold', '/development/generators/scaffolds/new', 1);
+
+
+INSERT INTO functional_areas (functional_area_name)
+VALUES ('Masterfiles');
+
+INSERT INTO programs (program_name, program_sequence, functional_area_id)
+VALUES ('Fruit', 1, (SELECT id FROM functional_areas WHERE functional_area_name = 'Masterfiles'));
+
+INSERT INTO program_functions (program_id, program_function_name, url, program_function_sequence)
+VALUES ((SELECT id FROM programs WHERE program_name = 'Fruit' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Masterfiles')), 'Commodity Groups', '/list/commodity_groups', 1);
+
+INSERT INTO program_functions (program_id, program_function_name, url, program_function_sequence)
+VALUES ((SELECT id FROM programs WHERE program_name = 'Fruit' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Masterfiles')), 'Commodities', '/list/commodities', 2);

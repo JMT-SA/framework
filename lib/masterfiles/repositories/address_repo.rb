81ac2e6385f7ba0d7da08e866c9422ef92be00e1
James Silberbauer <jamessil@telkomsa.net>
2017-11-10 15:25:48 +0200

    Address with type.

--- lib/masterfiles/repositories/address_repo.rb
+++ lib/masterfiles/repositories/address_repo.rb
@@ -8,4 +8,12 @@ class AddressRepo < RepoBase
                        value: :id,
                        order_by: :address_line_1
   end
+
+  def find_with_type(id)
+    AddressWithType.new(DB[main_table_name].join(:address_types, id: :address_type_id).where(Sequel[main_table_name][:id] => id).first)
+  end
+
+  def all_with_type
+    DB[main_table_name].join(:address_types, id: :address_type_id).map { |a| AddressWithType.new(a) }
+  end
 end

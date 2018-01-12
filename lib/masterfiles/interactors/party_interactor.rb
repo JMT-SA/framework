3cf7389b45cb4a8658efc45903a457db2fc77e1c
Violet <heilahendriks1@gmail.com>
2018-01-12 13:42:36 +0200

    tests

--- lib/masterfiles/interactors/party_interactor.rb
+++ lib/masterfiles/interactors/party_interactor.rb
@@ -5,14 +5,14 @@ class PartyInteractor < BaseInteractor
     DB.transaction do
       party_repo.link_addresses(id, address_ids)
     end
-    success_response('Addresses linked successfully', party)
+    success_response('Addresses linked successfully')
   end
 
   def link_contact_methods(id, contact_method_ids)
     DB.transaction do
       party_repo.link_contact_methods(id, contact_method_ids.uniq)
     end
-    success_response('Contact methods linked successfully', party)
+    success_response('Contact methods linked successfully')
   end
 
   private

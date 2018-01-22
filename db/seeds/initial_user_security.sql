a837f537517e52d26925cfd3e4a561929029c4d7
James Silberbauer <jamessil@telkomsa.net>
2018-01-22 09:31:24 +0200

    Add Development menu to DB seeds.

--- db/seeds/initial_user_security.sql
+++ db/seeds/initial_user_security.sql
@@ -15,5 +15,9 @@ VALUES ((SELECT id FROM users ORDER BY id LIMIT 1),
 
 INSERT INTO programs_users (user_id, program_id, security_group_id)
 VALUES ((SELECT id FROM users ORDER BY id LIMIT 1),
+  (SELECT id FROM programs WHERE program_name = 'Masterfiles' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Development')),
+  (SELECT id FROM security_groups g WHERE g.security_group_name = 'basic'));
+INSERT INTO programs_users (user_id, program_id, security_group_id)
+VALUES ((SELECT id FROM users ORDER BY id LIMIT 1),
   (SELECT id FROM programs WHERE program_name = 'Fruit' AND functional_area_id = (SELECT id FROM functional_areas WHERE functional_area_name = 'Masterfiles')),
   (SELECT id FROM security_groups g WHERE g.security_group_name = 'basic'));

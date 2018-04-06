a1bc4716d6834c14a8e159c844ae14041918d9d9
James Silberbauer <jamessil@telkomsa.net>
2018-04-06 21:07:20 +0200

    Dataminer creator can manage postgres schemas.

--- lib/dataminer/interactors/dm_creator.rb
+++ lib/dataminer/interactors/dm_creator.rb
@@ -1,21 +1,25 @@
 class DmCreator
   attr_reader :report, :db
 
-  def initialize(db, report)
-    @db     = db
+  def initialize(db_conn, report)
+    @db     = db_conn
     @report = report
   end
 
   def modify_column_datatypes
     column_names.each do |name|
-      report.columns[name].data_type = column_datatypes[name]
-      report.columns[name].groupable = groupable?(column_datatypes[name], name)
-      report.columns[name].format    = :delimited_1000 if column_datatypes[name] == :number
+      column_attributes_for(report.columns, name)
     end
     report
   end
 
   private
+
+  def column_attributes_for(columns, name)
+    columns[name].data_type = column_datatypes[name]
+    columns[name].groupable = groupable?(column_datatypes[name], name)
+    columns[name].format    = :delimited_1000 if column_datatypes[name] == :number
+  end
 
   def column_names
     report.columns.keys.reject { |name| column_datatypes[name].nil? }
@@ -37,26 +41,34 @@ class DmCreator
   end
 
   def column_datatypes
-    @datatypes ||= begin
-      tables       = report.tables
-      # puts ">>> #{tables.inspect}"
+    @column_datatypes ||= begin
       column_types = {}
-      tables.each do |table|
-        # puts ">>> TABLE: #{table}"
-        db.schema(table.sub('public.', '')).each do |col| # NB problem if another schema used... ... db.connection == Sequel.connection from ROM.
-          # puts ">>> COL: #{col}"
-          type = col[1][:type]
-          column_types[col.first.to_s] = case type # translate into :number etc...
-                                         when :decimal, :float
-                                           :number
-                                         else
-                                           type
-                                         end
-          # Check types with DB.schema('parties_roles').map {|c| c[1][:type] }.compact.uniq.sort
-          # :boolean, :date, :datetime, :decimal, :float, :integer, :string
-        end
+      report.tables.each do |full_table_name|
+        add_column_types_for(full_table_name, column_types)
       end
       column_types
     end
+  end
+
+  def add_column_types_for(full_table_name, column_types)
+    schema, table = split_schema_table(full_table_name)
+    db.schema(db.from(Sequel[schema][table])).each do |col|
+      column_types[col.first.to_s] = make_type(col[1][:type])
+    end
+  end
+
+  def make_type(type)
+    if %i[decimal float].include?(type)
+      :number
+    else
+      type
+    end
+  end
+
+  def split_schema_table(full_table_name)
+    ar = full_table_name.split('.')
+    table = ar.delete_at(-1)
+    schema = ar.first || 'public'
+    [schema.to_sym, table]
   end
 end

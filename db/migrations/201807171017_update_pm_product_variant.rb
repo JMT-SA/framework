25fe601d655461370f4d39013d956985752894b4
Violet <heilahendriks1@gmail.com>
2018-07-19 16:25:56 +0200

    ref dim fix

--- db/migrations/201807171017_update_pm_product_variant.rb
+++ db/migrations/201807171017_update_pm_product_variant.rb
@@ -7,10 +7,18 @@ Sequel.migration do
       drop_column(:standard)
       add_column(:product_variant_number, Integer)
       add_column(:active, TrueClass)
+      add_column(:reference_dimension, String)
+      drop_column(:length_mm)
+      drop_column(:width_mm)
+      drop_column(:height_mm)
+      drop_column(:diameter_mm)
+      drop_column(:thick_mm)
+      drop_column(:thick_mic)
     end
 
     alter_table(:pack_material_products) do
       add_column(:active, TrueClass)
+      add_column(:reference_dimension, String)
       drop_column(:length_mm)
       drop_column(:width_mm)
       drop_column(:height_mm)
@@ -25,10 +33,18 @@ Sequel.migration do
       add_column(:standard, TrueClass)
       drop_column(:product_variant_number)
       drop_column(:active)
+      drop_column(:reference_dimension)
+      add_column(:length_mm, Numeric)
+      add_column(:width_mm, Numeric)
+      add_column(:height_mm, Numeric)
+      add_column(:diameter_mm, Numeric)
+      add_column(:thick_mm, Numeric)
+      add_column(:thick_mic, Numeric)
     end
 
     alter_table(:pack_material_products) do
       drop_column(:active)
+      drop_column(:reference_dimension)
       add_column(:length_mm, Numeric)
       add_column(:width_mm, Numeric)
       add_column(:height_mm, Numeric)

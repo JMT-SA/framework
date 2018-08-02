020a76f361e92b9921d2b0776989119fbc176b73
James Silberbauer <jamessil@telkomsa.net>
2018-08-02 07:59:16 +0200

    Add and configure Capistrano.

---
+++ config/deploy/kromco.rb
@@ -0,0 +1,3 @@
+# Dataminer at Kromco.
+server '172.16.16.4', user: 'nsld', roles: %w[app db web]
+set :deploy_to, '/home/nsld/pack_materials'
---
+++ config/deploy/nosoftvm27.rb
@@ -0,0 +1,4 @@
+# VM instance at NoSoft offices
+server '192.168.50.27', user: 'nsld', roles: %w[app db web]
+set :deploy_to, '/home/nsld/pack_materials'
+# set :default_env, 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/run/passenger-instreg'

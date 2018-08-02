90ba7fbb5f1dea968ec1461faf72cf1c5c3dc1b4
James Silberbauer <jamessil@telkomsa.net>
2018-08-02 17:21:16 +0200

    Add assets to SCM.

---
+++ public/js/ag-enterprise-activation.js
@@ -0,0 +1,2 @@
+// AG Grid enterprise license:
+agGrid.LicenseManager.setLicenseKey('J&J_Multi-Tier_Kromco_Packhouse_solution_1Devs15_August_2018__MTUzNDI4NzYwMDAwMA==b0bd53ae86f33e9bd6d56f7992da0d10');

398639ba2ebbd31541ceb6801d23711125df8e81
James Silberbauer <jamessil@telkomsa.net>
2018-10-30 14:01:18 +0200

    Arithmetic for days and weeks in UtilityFunctions.

--- helpers/utility_functions.rb
+++ helpers/utility_functions.rb
@@ -1,6 +1,65 @@
 module UtilityFunctions
   module_function
 
+  TIME_DAY = 60 * 60 * 24
+  TIME_WEEK = 60 * 60 * 24 * 7
+
+  def weeks_ago(anchor, no_weeks)
+    raise ArgumentError unless no_weeks.positive?
+    case anchor
+    when Time
+      anchor - (no_weeks * TIME_WEEK)
+    when DateTime
+      anchor - (no_weeks * 7)
+    when Date
+      anchor - (no_weeks * 7)
+    else
+      raise ArgumentError, "weeks_ago: #{anchor.class} is not a date or time"
+    end
+  end
+
+  def weeks_since(anchor, no_weeks)
+    raise ArgumentError unless no_weeks.positive?
+    case anchor
+    when Time
+      anchor + (no_weeks * TIME_WEEK)
+    when DateTime
+      anchor + (no_weeks * 7)
+    when Date
+      anchor + (no_weeks * 7)
+    else
+      raise ArgumentError, "weeks_since: #{anchor.class} is not a date or time"
+    end
+  end
+
+  def days_ago(anchor, no_days)
+    raise ArgumentError unless no_days.positive?
+    case anchor
+    when Time
+      anchor - (no_days * TIME_WEEK)
+    when DateTime
+      anchor - no_days
+    when Date
+      anchor - no_days
+    else
+      raise ArgumentError, "days_ago: #{anchor.class} is not a date or time"
+    end
+  end
+
+  def days_since(anchor, no_days)
+    raise ArgumentError unless no_days.positive?
+    case anchor
+    when Time
+      anchor + (no_days * TIME_WEEK)
+    when DateTime
+      anchor + no_days
+    when Date
+      anchor + no_days
+    else
+      raise ArgumentError, "days_since: #{anchor.class} is not a date or time"
+    end
+  end
+
   def newline_and_spaces(count)
     "\n#{' ' * count}"
   end

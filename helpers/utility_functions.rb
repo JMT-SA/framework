4dbf8602bcf70cc40c0f18cc07de22cddd6b88da
James Silberbauer <jamessil@telkomsa.net>
2018-10-31 11:44:47 +0200

    Docs for days/weeks ago/since.

--- helpers/utility_functions.rb
+++ helpers/utility_functions.rb
@@ -5,58 +5,47 @@ module UtilityFunctions
   TIME_WEEK = 60 * 60 * 24 * 7
 
   def weeks_ago(anchor, no_weeks)
-    raise ArgumentError unless no_weeks.positive?
-    case anchor
-    when Time
-      anchor - (no_weeks * TIME_WEEK)
-    when DateTime
-      anchor - (no_weeks * 7)
-    when Date
-      anchor - (no_weeks * 7)
-    else
-      raise ArgumentError, "weeks_ago: #{anchor.class} is not a date or time"
-    end
+    change_weeks(anchor, no_weeks, -1)
   end
 
   def weeks_since(anchor, no_weeks)
-    raise ArgumentError unless no_weeks.positive?
-    case anchor
-    when Time
-      anchor + (no_weeks * TIME_WEEK)
-    when DateTime
-      anchor + (no_weeks * 7)
-    when Date
-      anchor + (no_weeks * 7)
-    else
-      raise ArgumentError, "weeks_since: #{anchor.class} is not a date or time"
-    end
+    change_weeks(anchor, no_weeks, 1)
   end
 
   def days_ago(anchor, no_days)
-    raise ArgumentError unless no_days.positive?
-    case anchor
-    when Time
-      anchor - (no_days * TIME_WEEK)
-    when DateTime
-      anchor - no_days
-    when Date
-      anchor - no_days
-    else
-      raise ArgumentError, "days_ago: #{anchor.class} is not a date or time"
-    end
+    change_days(anchor, no_days, -1)
   end
 
   def days_since(anchor, no_days)
+    change_days(anchor, no_days, 1)
+  end
+
+  def change_weeks(anchor, no_weeks, up_down)
+    raise ArgumentError unless no_weeks.positive?
+
+    case anchor
+    when Time
+      anchor + (no_weeks * TIME_WEEK * up_down)
+    when DateTime
+      anchor + (no_weeks * 7 * up_down)
+    when Date
+      anchor + (no_weeks * 7 * up_down)
+    else
+      raise ArgumentError, "change_weeks: #{anchor.class} is not a date or time"
+    end
+  end
+
+  def change_days(anchor, no_days, up_down)
     raise ArgumentError unless no_days.positive?
     case anchor
     when Time
-      anchor + (no_days * TIME_WEEK)
+      anchor + (no_days * TIME_DAY * up_down)
     when DateTime
-      anchor + no_days
+      anchor + (no_days * up_down)
     when Date
-      anchor + no_days
+      anchor + (no_days * up_down)
     else
-      raise ArgumentError, "days_since: #{anchor.class} is not a date or time"
+      raise ArgumentError, "change_days: #{anchor.class} is not a date or time"
     end
   end
 

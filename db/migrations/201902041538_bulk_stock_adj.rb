6d28f0d10ece0f32d378d85dfdfb5e27ab342708
Violet <heilahendriks1@gmail.com>
2019-02-04 17:12:17 +0200

    Bulk Stock Adjustment - migration only

---
+++ db/migrations/201902041538_bulk_stock_adj.rb
@@ -0,0 +1,89 @@
+require 'sequel_postgresql_triggers'
+Sequel.migration do
+  up do
+    extension :pg_triggers
+
+    run 'CREATE SEQUENCE doc_seqs_stock_adjustment_number;'
+    create_table(:mr_bulk_stock_adjustments, ignore_index_errors: true) do
+      primary_key :id
+      Integer :stock_adjustment_number, null: false, default: Sequel.function(:nextval, 'doc_seqs_stock_adjustment_number')
+      column :sku_numbers, 'integer[]'
+      String :location_codes #StringArray
+      TrueClass :active, default: true
+      TrueClass :is_stock_take, default: false
+
+      String :approved_by
+      DateTime :approved_on
+
+      DateTime :completed
+      DateTime :uncompleted_on
+
+      DateTime :created_at, null: false
+      DateTime :updated_at, null: false
+    end
+    run "SELECT audit.audit_table('mr_bulk_stock_adjustments', true, true,'{updated_at}'::text[]);"
+    pgt_created_at(:mr_bulk_stock_adjustments,
+                   :created_at,
+                   function_name: :mr_bulk_stock_adjustments_set_created_at,
+                   trigger_name: :set_created_at)
+
+    pgt_updated_at(:mr_bulk_stock_adjustments,
+                   :updated_at,
+                   function_name: :mr_bulk_stock_adjustments_set_updated_at,
+                   trigger_name: :set_updated_at)
+
+    create_table(:mr_bulk_stock_adjustment_items, ignore_index_errors: true) do
+      primary_key :id
+      foreign_key :mr_bulk_stock_adjustment_id, :mr_bulk_stock_adjustments, null: false, key: [:id]
+      foreign_key :mr_sku_location_id, :mr_sku_locations, null: false, key: [:id]
+      Integer :sku_number, null: false
+      Integer :product_variant_number
+      Integer :product_number
+
+      String :mr_type_name, null: false
+      String :mr_sub_type_name, null: false
+      String :product_variant_code, null: false
+      String :product_code, null: false
+      String :location_code
+      String :inventory_uom_code
+      String :scan_to_location_code
+
+      BigDecimal :system_quantity, size: [7, 2]
+      BigDecimal :actual_quantity, size: [7, 2]
+
+      TrueClass :stock_take_complete, default: false
+      TrueClass :active, default: true
+
+      DateTime :created_at, null: false
+      DateTime :updated_at, null: false
+
+      index [:mr_bulk_stock_adjustment_id], name: :fki_mr_bulk_stock_adjustment_items_mr_bulk_stock_adjustments
+      index [:mr_sku_location_id], name: :fki_mr_bulk_stock_adjustment_items_mr_sku_locations
+    end
+    run "SELECT audit.audit_table('mr_bulk_stock_adjustment_items', true, true,'{updated_at}'::text[]);"
+    pgt_created_at(:mr_bulk_stock_adjustment_items,
+                   :created_at,
+                   function_name: :mr_bulk_stock_adjustment_items_set_created_at,
+                   trigger_name: :set_created_at)
+
+    pgt_updated_at(:mr_bulk_stock_adjustment_items,
+                   :updated_at,
+                   function_name: :mr_bulk_stock_adjustment_items_set_updated_at,
+                   trigger_name: :set_updated_at)
+  end
+
+  down do
+    drop_trigger(:mr_bulk_stock_adjustment_items, :set_created_at)
+    drop_function(:mr_bulk_stock_adjustment_items_set_created_at)
+    drop_trigger(:mr_bulk_stock_adjustment_items, :set_updated_at)
+    drop_function(:mr_bulk_stock_adjustment_items_set_updated_at)
+    drop_table(:mr_bulk_stock_adjustment_items)
+
+    drop_trigger(:mr_bulk_stock_adjustments, :set_created_at)
+    drop_function(:mr_bulk_stock_adjustments_set_created_at)
+    drop_trigger(:mr_bulk_stock_adjustments, :set_updated_at)
+    drop_function(:mr_bulk_stock_adjustments_set_updated_at)
+    drop_table(:mr_bulk_stock_adjustments)
+    run 'DROP SEQUENCE doc_seqs_stock_adjustment_number;'
+  end
+end

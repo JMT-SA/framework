424cb578b7b8eaf64951c015459f271a8b2601a1
Heila767 <heilahendriks1@gmail.com>
2019-07-18 16:39:46 +0200

    rearrange transaction history, re-hide adhoc, fix

--- lib/pack_material/entities/mr_bulk_stock_adjustment.rb
+++ lib/pack_material/entities/mr_bulk_stock_adjustment.rb
@@ -6,6 +6,7 @@ module PackMaterialApp
     attribute :stock_adjustment_number, Types::Integer
     attribute :create_transaction_id, Types::Integer
     attribute :destroy_transaction_id, Types::Integer
+    attribute :business_process_id, Types::Integer
     attribute :is_stock_take, Types::Bool
     attribute :completed, Types::Bool
     attribute :approved, Types::Bool

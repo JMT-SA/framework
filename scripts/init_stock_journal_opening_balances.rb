df84b298ee6e5126c5e0010a21138ec1a225c5e5
Heila767 <heilahendriks1@gmail.com>
2020-06-24 12:45:05 +0200

    Update Script to future proof it for reruns

--- scripts/init_stock_journal_opening_balances.rb
+++ scripts/init_stock_journal_opening_balances.rb
@@ -5,10 +5,10 @@ class InitStockJournalOpeningBalances < BaseScript
   def run # rubocop:disable Metrics/AbcSize
     sql_info = ['Initiate opening balances for Periodic Stock Report.']
 
-    dates = DB["select generate_series(('2019_07_01')::date, ('2020_06_01')::date, interval '1 month');"].all.map { |r| r[:generate_series].strftime('%Y-%m-%d') }
+    dates = DB["select generate_series(date_trunc('month', date('#{AppConst::STOCK_TAKE_ON_DATE}'))::date, date_trunc('month', current_date)::date, interval '1 month');"].all.map { |r| r[:generate_series].strftime('%Y-%m-%d') }
     dates.each do |date|
       script = %(INSERT INTO stock_journal_entries (mr_product_variant_id, opening_balance, opening_balance_on)
-                  SELECT mrpv.id, fn_stock_total_at(mrpv.id, ('2019_01_01')::date, ('#{date}')::date), ('#{date}')::date
+                  SELECT mrpv.id, fn_stock_total_at(mrpv.id, date_trunc('month', date('#{AppConst::STOCK_TAKE_ON_DATE}'))::date, ('#{date}')::date), ('#{date}')::date
                   FROM material_resource_product_variants mrpv;)
       sql_info << script
       if debug_mode

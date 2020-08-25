4bbbe8469bee286635e21f85743dd5b472beda8c
James Silberbauer <jamessil@telkomsa.net>
2020-08-25 10:43:38 +0200

    Use opening_balance_on, not opening_balance_at

--- lib/pack_material/repositories/stock_movement_report_repo.rb
+++ lib/pack_material/repositories/stock_movement_report_repo.rb
@@ -7,15 +7,15 @@ module PackMaterialApp
                  mrpv.product_variant_code,
                  (select sje.opening_balance
                   from stock_journal_entries sje
-                  where sje.opening_balance_at < ('#{start_date}')::date and mrpv.id = sje.mr_product_variant_id
-                  order by opening_balance_at desc limit 1),
+                  where sje.opening_balance_on < ('#{start_date}')::date and mrpv.id = sje.mr_product_variant_id
+                  order by opening_balance_on desc limit 1),
                  fn_stock_quantity_received(mrpv.id, ('#{start_date}')::date, ('#{end_date}')::date) as quantity_received,
                  fn_stock_quantity_returned(mrpv.id, ('#{start_date}')::date, ('#{end_date}')::date) as quantity_returned,
                  fn_stock_quantity_sold(mrpv.id, ('#{start_date}')::date, ('#{end_date}')::date) as quantity_sold,
                  (select (sje.opening_balance + fn_stock_total_at(mrpv.id,('#{start_date}')::date,('#{end_date}')::date))
                   from stock_journal_entries sje
-                  where sje.opening_balance_at < ('#{start_date}')::date and mrpv.id = sje.mr_product_variant_id
-                  order by opening_balance_at desc limit 1) as closing_balance
+                  where sje.opening_balance_on < ('#{start_date}')::date and mrpv.id = sje.mr_product_variant_id
+                  order by opening_balance_on desc limit 1) as closing_balance
                  from material_resource_product_variants mrpv"].all
     end
 
